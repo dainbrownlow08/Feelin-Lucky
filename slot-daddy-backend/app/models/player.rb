41dcc642486c0351aa7a97f732e224dd3449a9a0
Zoe Kirsh <68711764+zoekirsh@users.noreply.github.com>
2021-04-28 16:54:12 -0700

    Delete slot-daddy-backend directory

--- slot-daddy-backend/app/models/player.rb
+++
@@ -1,6 +0,0 @@
-class Player < ApplicationRecord
-    has_many :games
-    has_many :tokens
-
-    validates_uniqueness_of :username
-end
--- slot-daddy-backend/db/migrate/20210126013112_create_tokens.rb
+++
@@ -1,9 +0,0 @@
-class CreateTokens < ActiveRecord::Migration[6.0]
-  def change
-    create_table :tokens do |t|
-      t.references :player, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
